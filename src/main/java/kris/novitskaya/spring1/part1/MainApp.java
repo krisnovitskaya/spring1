a276a9c4f792869a8547245854ab94aff57742bc
Kris Novitskaya <voitage@yandex.ru>
2020-09-18 08:24:41 +0300

    clear master

--- src/main/java/kris/novitskaya/spring1/part1/MainApp.java
+++ src/main/java/kris/novitskaya/spring1/part1/MainApp.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class MainApp {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        context.close();
     }
 }
